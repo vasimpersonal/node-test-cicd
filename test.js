1d972277c827e8134e3aebc3716b396e228127c9
vasimpersonal <vaseem.farooqui27@gmail.com>
2024-07-04 19:54:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 // test.js
 const request = require('supertest');
 const express = require('express');
-const app = require('../app');
-
+const app = require('./app');
+ 
 describe('GET /', function() {
     it('responds with Hello World', function(done) {
         request(app)
